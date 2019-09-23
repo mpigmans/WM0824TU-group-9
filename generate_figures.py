9ba44b3372ee2c3479d24db6c7da5c027cbe29e1
Daniël Vos <daniel.vos.d@gmail.com>
2019-09-23 11:04:40 +0200

    Fix plot axis titles

--- generate_figures.py
+++ generate_figures.py
@@ -13,10 +13,12 @@ countries = {}
 for country in pycountry.countries:
     countries[country.name] = country.alpha_2
 
-# Figure 1: Amount of packets by country
+# Figure 1: Amount of infected hosts by country
 top_count = 20
 
-count_per_country_date = data.groupby("Country").size().reset_index(name="Packets")
+count_per_country_date = (
+    data.groupby("Country").size().reset_index(name="Infected Hosts")
+)
 users_per_country = users_per_country.rename(columns={"Country or Area": "Country"})
 users_per_country["Country"] = users_per_country["Country"].apply(
     lambda x: countries.get(x, "Unknown code")
@@ -29,40 +31,56 @@ combined_data["Internet Users"] = combined_data["Internet Users"].apply(
     lambda x: x.replace(",", "")
 )
 combined_data["Internet Users"] = pd.to_numeric(combined_data["Internet Users"])
-combined_data["Packets / Users"] = (
-    combined_data["Packets"] / combined_data["Internet Users"]
+combined_data["Infected Hosts / Internet Users"] = (
+    combined_data["Infected Hosts"] / combined_data["Internet Users"]
 )
 
-# Bar plot top countries by mirai packets normalized by internet users
+# Bar plot top countries by mirai infected hosts normalized by internet users
 sorted_countries = combined_data.sort_values(by="Internet Users", ascending=False)
 sorted_countries = sorted_countries.head(50)
-sorted_countries = sorted_countries.sort_values(by="Packets / Users", ascending=False)
+sorted_countries = sorted_countries.sort_values(
+    by="Infected Hosts / Internet Users", ascending=False
+)
 top_countries = sorted_countries.head(top_count)
-sns.barplot(data=top_countries, x="Country", y="Packets / Users", palette="GnBu_d")
+sns.barplot(
+    data=top_countries,
+    x="Country",
+    y="Infected Hosts / Internet Users",
+    palette="GnBu_d",
+)
 plt.tight_layout()
 plt.savefig("figures/infected_hosts_by_country")
 plt.close()
 
-# Scatter plot comparing country internet usage with amount of mirai packets
+# Scatter plot comparing country internet usage with amount of mirai infected hosts
 sorted_countries = combined_data.sort_values(by="Internet Users", ascending=False)
 sorted_countries = sorted_countries.head(15)
 ax = sns.scatterplot(
-    data=sorted_countries, x="Internet Users", y="Packets", hue="Country"
+    data=sorted_countries,
+    x="Internet Users",
+    y="Infected Hosts",
+    hue="Country",
+    palette="GnBu_d",
 )
 ax.set(xscale="log", yscale="log")
 plt.tight_layout()
 plt.savefig("figures/infected_hosts_by_country_scatter")
 plt.close()
 
-# Figure 2: Amount of packets over time
+# Figure 2: Amount of infected hosts over time
 dates = data["Date_First_Seen"]
 data_month_year = dates.apply(lambda date: (date.year, date.month))
 date_histogram = data_month_year.value_counts().sort_index().to_frame()
 date_histogram["Date"] = date_histogram.index
-date_histogram.columns = ["Packets", "Date"]
+date_histogram.columns = ["Infected Hosts", "Date"]
 date_histogram["Date"] = date_histogram["Date"].apply(lambda x: "{}-{}".format(*x))
 ax = sns.lineplot(
-    data=date_histogram, x="Date", y="Packets", palette="GnBu_d", marker="o", sort=False
+    data=date_histogram,
+    x="Date",
+    y="Infected Hosts",
+    palette="GnBu_d",
+    marker="o",
+    sort=False,
 )
 plt.xticks(rotation=35)
 plt.tight_layout()
