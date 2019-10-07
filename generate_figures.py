dd88c5817c090d9adab5634c88b36fd8375ad86d
Daniël Vos <daniel.vos.d@gmail.com>
2019-10-07 11:56:17 +0200

    Add final report block 3

--- generate_figures.py
+++ generate_figures.py
@@ -175,10 +175,10 @@ asns_to_plot = {
     "AS7922": "Comcast Cable Communications",
 }
 isp_to_size = {
-    "CenturyLink Communications": 437388658,
-    "AT&T Services": 363293945,
-    "Comcast Cable Communications": 152939901,
-}  # Data from https://asrank.caida.org/
+    "CenturyLink Communications": 20809984,
+    "AT&T Services": 83883008,
+    "Comcast Cable Communications": 71195904,
+}  # Data from https://ipinfo.io/
 data_date_asn = data[["Date_First_Seen", "ASN"]].copy(deep=True)
 data_date_asn = data_date_asn[data_date_asn["ASN"].isin(asns_to_plot.keys())]
 data_date_asn["ASN"] = data_date_asn["ASN"].map(asns_to_plot)
@@ -188,24 +188,101 @@ date_asn_sizes = (
 date_asn_sizes["Infected / Total Addresses"] = date_asn_sizes["Count"] / date_asn_sizes[
     "ASN"
 ].map(isp_to_size)
+date_asn_sizes["Infected / Million Total Addresses"] = (
+    date_asn_sizes["Infected / Total Addresses"] * 1000000
+)
 date_asn_sizes["Date"] = date_asn_sizes["Date_First_Seen"].apply(
     lambda x: "{}-{}".format(*x)
 )
 print(date_asn_sizes)
 sns.lineplot(
-    data=date_asn_sizes, x="Date", y="Infected / Total Addresses", hue="ASN", sort=False
+    data=date_asn_sizes,
+    x="Date",
+    y="Infected / Million Total Addresses",
+    hue="ASN",
+    sort=False,
 )
 plt.xticks(rotation=35)
 plt.tight_layout()
 plt.savefig("figures/infected_hosts_over_time_by_asn")
 plt.close()
 
+# Calculate comcast devices and ROSI
+gb_per_device_per_month = 74120
+gb_per_device_per_month_std = 23800
+cost_per_gb = 0.002
+attacking_device_fraction = 0.087
+attacking_device_fraction_std = 0.025
+color = "cornflowerblue"
+
+asns_to_plot = {"AS7922": "Comcast Cable Communications"}
+data_date_asn = data[["Date_First_Seen", "ASN"]].copy(deep=True)
+data_date_asn = data_date_asn[data_date_asn["ASN"].isin(asns_to_plot.keys())]
+data_date_asn["ASN"] = data_date_asn["ASN"].map(asns_to_plot)
+date_asn_sizes = (
+    data_date_asn.groupby(["Date_First_Seen", "ASN"])
+    .size()
+    .reset_index(name="Infected Devices")
+)
+date_asn_sizes["Date"] = date_asn_sizes["Date_First_Seen"].apply(
+    lambda x: "{}-{}".format(*x)
+)
+print(date_asn_sizes)
+sns.lineplot(data=date_asn_sizes, x="Date", y="Infected Devices", hue="ASN", sort=False)
+plt.xticks(rotation=35)
+plt.tight_layout()
+plt.savefig("figures/infected_comcast_hosts_over_time")
+plt.close()
+
+date_asn_sizes["Cost Expectation"] = (
+    date_asn_sizes["Infected Devices"] * attacking_device_fraction * gb_per_device_per_month * cost_per_gb
+)
+date_asn_sizes["Cost Upper"] = (
+    date_asn_sizes["Infected Devices"]
+    * (attacking_device_fraction + attacking_device_fraction_std)
+    * (gb_per_device_per_month + gb_per_device_per_month_std)
+    * cost_per_gb
+)
+date_asn_sizes["Cost Lower"] = (
+    date_asn_sizes["Infected Devices"]
+    * (attacking_device_fraction - attacking_device_fraction_std)
+    * (gb_per_device_per_month - gb_per_device_per_month_std)
+    * cost_per_gb
+)
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(len(date_asn_sizes)), date_asn_sizes["Cost Expectation"], color=color)
+ax.fill_between(
+    np.arange(len(date_asn_sizes)),
+    date_asn_sizes["Cost Expectation"],
+    y2=date_asn_sizes["Cost Upper"],
+    alpha=0.4,
+    color=color,
+)
+ax.fill_between(
+    np.arange(len(date_asn_sizes)),
+    date_asn_sizes["Cost Expectation"],
+    y2=date_asn_sizes["Cost Lower"],
+    alpha=0.4,
+    color=color,
+)
+ax.set_xticks(np.arange(len(date_asn_sizes)))
+ax.set_xticklabels(date_asn_sizes["Date"], rotation=40, ha="right")
+ax.set_xlabel("Date")
+ax.set_ylabel("Cost (USD)")
+plt.tight_layout()
+plt.savefig("figures/comcast_costs")
+plt.close()
+
+print("Expected loss:", date_asn_sizes.loc[0:18]["Cost Expectation"].sum())
+print("Upper loss:", date_asn_sizes.loc[0:18]["Cost Upper"].sum())
+print("Lower loss:", date_asn_sizes.loc[0:18]["Cost Lower"].sum())
+
 # Block 3 - Calculate ROSI
 worldwide_hosts = 1e9
 company_hosts = 100
 costs_mean = 2161409
 costs_std = 1400000
-color = "cornflowerblue"
 
 date_histogram.reset_index(inplace=True, drop=True)
 date_histogram["Infection Likelihood"] = (
