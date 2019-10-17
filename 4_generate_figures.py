3ed80fb026048b01d3a083d1c29c5159a0526728
Daniël Vos <daniel.vos.d@gmail.com>
2019-10-17 15:30:21 +0200

    Add correlation data, plots and analysis

--- 4_generate_figures.py
+++ 4_generate_figures.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 import pycountry
 import numpy as np
 import datetime
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
+import statsmodels.formula.api as smf
 
 # Set parameters for plot:
-start_date = datetime.datetime(2018, 1, 1)
-end_date = datetime.datetime(2018, 12, 31)
-# start_date = datetime.datetime(2010, 6, 1)
-# end_date = datetime.datetime(2020, 6, 7)
+start_date = datetime.datetime(2018, 6, 1)
+end_date = datetime.datetime(2018, 7, 1)
 
 # Data columns are as follows:
 # IP_Address ; Autonomous_System ; Country ; ASN ; Date_First_Seen
@@ -20,8 +19,10 @@ idi_data = pd.read_csv("idi_codes.csv")
 gdp_data = pd.read_csv("gdp_per_capita.csv")
 
 # Combine datasets
-data = data.loc[(data['Date_First_Seen'] > start_date) & (data['Date_First_Seen'] < end_date)]
-data.rename(columns={'Date_First_Seen': 'Date First Detected'})
+data = data.loc[
+    (data["Date_First_Seen"] > start_date) & (data["Date_First_Seen"] < end_date)
+]
+data.rename(columns={"Date_First_Seen": "Date First Detected"})
 count_per_country_date = (
     data.groupby("Country").size().reset_index(name="Infected Hosts")
 )
@@ -34,41 +35,50 @@ combined_data["Internet Users"] = combined_data["Internet Users"].apply(
     lambda x: x.replace(",", "")
 )
 combined_data["Internet Users"] = pd.to_numeric(combined_data["Internet Users"])
-combined_data["Infected Hosts / Internet Users"] = (
+combined_data["Infections"] = (
     combined_data["Infected Hosts"] / combined_data["Internet Users"]
 )
 
 # Load GCI data
 gci_data = pd.read_csv("gci.csv")
-gci_data = gci_data.rename(columns={"Member State": "Country", "Score": "Global Cybersecurity Index"})
+gci_data = gci_data.rename(columns={"Member State": "Country", "Score": "GCI"})
 gci_data = gci_data.drop(columns=["Global Rank"])
-combined_data = pd.merge(
-    combined_data, gci_data, on="Country", how="inner"
-)
-combined_data = pd.merge(
-    combined_data, gdp_data, on="Country", how="inner"
-)
-combined_data = pd.merge(
-    combined_data, idi_data, on="Country", how="inner"
-)
+combined_data = pd.merge(combined_data, gci_data, on="Country", how="inner")
+combined_data = pd.merge(combined_data, gdp_data, on="Country", how="inner")
+combined_data = pd.merge(combined_data, idi_data, on="Country", how="inner")
 
-correlation, p = pearsonr(combined_data['Infected Hosts / Internet Users'], combined_data['IDI'])
-print('IDI Correlation:', correlation, f'p={round(p, 4)}')
-sns.jointplot(x="IDI", y="Infected Hosts / Internet Users", data=combined_data, kind="reg")
+# Make infections normal distribution using logarithm
+combined_data["Infections"] = combined_data["Infections"].apply(np.log)
+
+correlation, p = pearsonr(combined_data["Infections"], combined_data["IDI"])
+print("IDI Correlation:", correlation, f"p={round(p, 4)}")
+correlation, p = spearmanr(combined_data["Infections"], combined_data["IDI"])
+print("IDI Spearman:", correlation, f"p={round(p, 4)}")
+sns.jointplot(x="IDI", y="Infections", data=combined_data, kind="reg")
 plt.tight_layout()
-plt.savefig('figures/correlation_idi')
+plt.savefig("figures/correlation_idi")
 plt.close()
 
-correlation, p = pearsonr(combined_data['Infected Hosts / Internet Users'], combined_data['Global Cybersecurity Index'])
-print('GCI Correlation:', correlation, f'p={round(p, 4)}')
-sns.jointplot(x="Global Cybersecurity Index", y="Infected Hosts / Internet Users", data=combined_data, kind="reg")
+correlation, p = pearsonr(combined_data["Infections"], combined_data["GCI"])
+print("GCI Correlation:", correlation, f"p={round(p, 4)}")
+correlation, p = spearmanr(combined_data["Infections"], combined_data["GCI"])
+print("GCI Spearman:", correlation, f"p={round(p, 4)}")
+sns.jointplot(x="GCI", y="Infections", data=combined_data, kind="reg")
 plt.tight_layout()
-plt.savefig('figures/correlation_gci')
+plt.savefig("figures/correlation_gci")
 plt.close()
 
-correlation, p = pearsonr(combined_data['Infected Hosts / Internet Users'], combined_data['GDP per capita'])
-print('GDP Correlation:', correlation, f'p={round(p, 4)}')
-sns.jointplot(x="GDP per capita", y="Infected Hosts / Internet Users", data=combined_data, kind="reg")
+correlation, p = pearsonr(combined_data["Infections"], combined_data["GDP"])
+print("GDP Correlation:", correlation, f"p={round(p, 4)}")
+correlation, p = spearmanr(combined_data["Infections"], combined_data["GDP"])
+print("GDP Spearman:", correlation, f"p={round(p, 4)}")
+sns.jointplot(x="GDP", y="Infections", data=combined_data, kind="reg")
 plt.tight_layout()
-plt.savefig('figures/correlation_gdp')
+plt.savefig("figures/correlation_gdp")
 plt.close()
+
+# Extract X and y from dataframe and fit OLS with combined metrics
+formula = (
+    "Infections ~ IDI + GCI + GDP + IDI * GCI + GCI * GDP + IDI * GDP + IDI * GCI * GDP"
+)
+print(smf.ols(formula, data=combined_data).fit().summary())
---
+++ convert.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pycountry
+
+def country_to_code(country_name):
+    try:
+        return pycountry.countries.search_fuzzy(country_name)[0].alpha_2
+    except LookupError:
+        return country_name
+
+# gci_data = pd.read_csv("gci.csv")
+# gci_data['Member State'] = gci_data['Member State'].apply(country_to_code)
+# gci_data.to_csv('gci_codes.csv', index=False)
+# print(gci_data['Member State'].value_counts())
+
+# gci_data = pd.read_csv("internet_users.csv")
+# gci_data['Country or Area'] = gci_data['Country or Area'].apply(country_to_code)
+# gci_data.to_csv('internet_users_codes.csv', index=False)
+# print(gci_data['Country or Area'].value_counts())
+
+# gci_data = pd.read_csv("gdp_per_capita.csv")
+# # gci_data['Country'] = gci_data['Country'].apply(country_to_code)
+# # gci_data = gci_data.drop(columns=['Country Code'])
+# print(gci_data)
+# gci_data = gci_data.dropna()
+# print(gci_data)
+# gci_data.to_csv('gdp_per_capita_codes.csv', index=False)
+# # print(gci_data['Country'].value_counts())
+
+gci_data = pd.read_csv("idi.csv")
+gci_data['Country'] = gci_data['Country'].apply(country_to_code)
+gci_data.to_csv('idi_codes.csv', index=False)
+print(gci_data['Country'].value_counts())
